bbd0cffd7a9c76bd411581c9a36e0cf60c1de2ca
AMCGUIMA <anderson.m_guimaraes@hotmail.com>
2025-04-26 00:08:54 -0300

    ESTAGIO NOVATO CONCLUIDO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,5 +36,74 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+    //DECLARAÇAO DA MATRIZ 5X5 QUE REPRESENTARÁ O TABULEIRO E INICIALIZADO COM ZERA PARA REPRESENTAR AGUA
+    int tabuleiro[5][5] = {
+        {0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0 }
+    };
+
+
+    //DECLARADO E INICIALIZADO DOIS VETORES QUE REPRESENTARAR OS NAVIOS COM O VALOR 3 DE CADA POSIÇÃO DO VETOR
+    int navio_1[3]={3, 3, 3};
+    int navio_2[3]={3, 3, 3};
+
+    //COORDENADAS       X  Y
+    int coordenada1[2]={1, 1};
+    int coordenada2[2]={5, 4};
+ //      0  1  2  3  4  -->POSIÇOES NA MATRIZ
+ //   | --------->  Y
+ // 0 | {0, 0, 0, 0, 0 },
+ // 1 | {0, 0, 0, 0, 0 },
+ // 2 | {0, 0, 0, 0, 0 },
+ // 3 X {0, 0, 0, 0, 0 },
+ // 4   {0, 0, 0, 0, 0 }
+ 
+// CONDIÇÃO QUE VALIDA SE AS COORDENADAS SETADAS ESTAO DENTRO DO LIMITE DO TABULEIRO.
+    if(coordenada1[0] < 5 && coordenada1[1] < 5 && coordenada2[0] < 5 && coordenada2[1] < 5)
+    {
+
+        // VARRE O TABULEIRO (MATRIZ ) HORIZONTALMENTE DE ACORDO COM A COORDENADA SETADA NAS VARIAVEIS E COPIA O VALOR DA POSICAO DO VETOR 
+        for (int y = coordenada1[1]; y < (3 + coordenada1[1]); y++)
+        {
+            tabuleiro[coordenada1[0]][y] = navio_1[(y - coordenada1[1])];
+        }
+    
+        // VARRE O TABULEIRO (MATRIZ )VERTICALMENTE DE ACORDO COM A COORDENADA SETADAS NAS VARIAVEIS E COPIA O VALOR DA POSICAO DO VETOR PARA A POSICAO DA MATRIZ
+        for (int x = coordenada2[0]; x < (3 + coordenada2[0]); x++)
+        {
+            tabuleiro[x][coordenada2[1]]= navio_1[(x - coordenada2[0])];
+            
+        }
+
+        printf("\nTABULEIRO - BATALHA NAVAL\n\n");
+        //LOOP PARA MOSTRAR NA TELA OS VALORES DAS POSICOES DO TABULEIRO
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                printf("%d ",tabuleiro[x][y]);
+            }
+            printf("\n");
+            
+        }
+
+    }//QUANDO A CONDIÇAO É FALSA -- VERIFICA QUAL COORDENADA PASSOU DO LIMITE E PASSA A MESAGEM PARA CORRIGIR
+    else{
+            if(coordenada1[0] >= 5 || coordenada1[1] >=5) // CODIÇÃO QUE VALIDA SE UMA DAS DUAS POSIÇOES DA COORDENADA PASSOU DO LIMITE DO TABULEIRO
+            {
+                printf("\n\nCoordenada 1 [x][y] PASSOU DO LIMITE DO TABULEIRO!!\n Necessario Corrigir\n\n");
+                
+            }
+            if(coordenada2[0] >=5 || coordenada2[1] >=5)// CODIÇÃO QUE VALIDA SE UMA DAS DUAS POSIÇOES DA COORDENADA PASSOU DO LIMITE DO TABULEIRO
+            {
+                printf("\n\nCoordenada 2 [x][y] PASSOU DO LIMITE DO TABULEIRO!!\n Necessario Corrigir\n\n");
+                    
+             }
+        }
+
+
     return 0;
 }
